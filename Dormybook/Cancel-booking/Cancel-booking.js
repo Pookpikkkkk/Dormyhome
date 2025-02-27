91406186712123e85faea1c421f24e54f19a3280
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-27 21:23:09 +0700

    0

---
+++ Dormybook/Cancel-booking/Cancel-booking.js
@@ -0,0 +1,4 @@
+document.getElementById('dropdownButton').addEventListener('click', function() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.classList.toggle('hidden');
+});
