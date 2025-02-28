dceea18029addfec7d4d58125879af645caf81c8
Apinya Sanghong <67023008@up.ac.th>
2025-02-28 21:12:58 +0700

    อัพ

--- Dormybook/arrivingsoon/arrivingsoon.js
+++ Dormybook/arrivingsoon/arrivingsoon.js
@@ -9,16 +9,3 @@ document.querySelector('.input-condition').addEventListener('change', function()
         window.location.href = "#";
     }
 });
-function loadImagesOnOtherPage() {
-    for (let i = 1; i <= 3; i++) {
-        const previewId = `preview${i}`;
-        const savedImage = localStorage.getItem(previewId);
-        if (savedImage) {
-            const imageContainer = document.getElementById(previewId);
-            if (imageContainer) {
-                imageContainer.innerHTML = `<img src="${savedImage}" alt="Saved Image" style="max-width: 100%; height: auto;">`;
-            }
-        }
-    }
-}
-window.onload = loadImagesOnOtherPage;
